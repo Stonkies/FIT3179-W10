67786903d6d9cd88922de11eb3d78086be6b3c9c
Noah <78777372+Stonkies@users.noreply.github.com>
2024-10-08 18:46:14 +1100

    Visualisation files

---
+++ homework_w10.js
@@ -0,0 +1,9 @@
+var vg_1 = "homework_w10.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var vg_2 = "homework_w9_copy.vg.json";
+vegaEmbed("#map2", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
